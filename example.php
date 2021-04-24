8d3775cdc7e05fd9074c589f14d474726df051cd
calledit <1573053+calledit@users.noreply.github.com>
2021-04-24 15:06:12 +0200

    Added some comments and an example form link

--- example.php
+++ example.php
@@ -1,10 +1,14 @@
 <?php
-include("index.php");
+//the url to the google form
+$_GET['url'] = 'https://docs.google.com/forms/d/e/1FAIpQLScJCYDMZB9jAAmlO5OIx740nWg3WrVDrkb9nL8XF1_BE9b-Vg/viewform';
+include("index.php");//include the google form 
 ?>
 <style>
+  /* make the backgound white*/
 .freebirdLightBackground,body{
     background-color: white !important
 }
+  /*Hide banners*/
 .freebirdFormviewerViewFooterDisclaimer,
 	.freebirdFormviewerViewFooterPageBreak,
 	.freebirdFormviewerViewHeaderThemeStripe,
@@ -14,6 +18,7 @@ include("index.php");
 }
 </style>
 <script>
+  //Show alert buttons when the user tries to continue to the next page
 setTimeout(function(){
 	var navigator_buttons = document.querySelectorAll('.freebirdFormviewerViewNavigationNoSubmitButton');
 	for(i=0;i<navigator_buttons.length;i++){
